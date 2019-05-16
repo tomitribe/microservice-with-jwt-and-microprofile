9532536cb9053de5dcc344f543c58fe6e6867887
Andrii Malytskyi <dexteritymaster@gmail.com>
2019-05-16 21:43:15 +0300

    #47 & #48 Refresh and logout sequence clean-ups

--- src/main/webapp/app/js/model/auth.js
+++ src/main/webapp/app/js/model/auth.js
@@ -41,171 +41,135 @@
                 refresh_exp: ''
             },
             loginModel: null,
-            ref: null,
             chRef: null,
             initialize: function () {
                 var me = this;
                 me.loginModel = new LoginModel();
-                me.ref = _.throttle(function () {
-                    if (!me.refreshActive) me.refreshRunner();
-                }, 1000);
                 me.chRef = _.throttle(me.checkRefresh, 500);
-
-                $.ajaxSetup({
+                // Simplest way to inject Authorization header for jQuery
+                /*$.ajaxSetup({
                     beforeSend: function (jqXHR) {
                         var access_token = me.get('access_token'), token_type = me.get('token_type') + " ";
                         if (typeof access_token !== 'undefined' && !!access_token) {
                             jqXHR.setRequestHeader('Authorization', token_type + access_token);
                         }
-                        me.chRef(jqXHR);
                     }
-                });
+                });*/
 
                 $.ajaxTransport("+*", function (options, originalOptions, jqXHR) {
+                    me.chRef();
                     if (!originalOptions.ignoreTransport) {
                         const transport = {
                             options,
                             originalOptions,
                             jqXHR,
                             cb: null,
+                            abort: function (message) {
+                                this.cb(400, message || 'request failed');
+                            },
                             send: function (retryRequest) {
-                                const refresh_exp = me.get('refresh_exp');
+                                if (me.loggingOut) return this.abort();
+
+                                // Another way to inject Authorization header
+                                const access_token = me.get('access_token'), token_type = me.get('token_type') + " ";
+                                if (typeof access_token !== 'undefined' && !!access_token) {
+                                    this.originalOptions.headers = {
+                                        ...this.originalOptions.headers,
+                                        authorization: token_type + access_token
+                                    };
+                                }
                                 $.ajax({
                                     ...this.originalOptions,
                                     ignoreTransport: true,
                                     retryRequest
                                 }).done((data, status, xhr) => {
-                                    //console.log(data, status, xhr);
                                     this.cb(200, status, {text: xhr.responseText, JSON: xhr.responseJSON});
                                 }).fail(xhr => {
-                                    //console.log(xhr);
+                                    if (me.loggingOut) return this.abort();
+
                                     const now = moment().valueOf(),
-                                        leftRe = refresh_exp && (refresh_exp - now);
-
-                                    if (me.checkRefStatus) {
+                                        refresh_exp = me.get('refresh_exp'),
+                                        leftRe = (refresh_exp - now);
+                                    if (!refresh_exp || leftRe < 0) {
+                                        me.logoutAndWarn(xhr);
+                                    } else if (me.checkRefStatus) {
                                         setTimeout(() => {
-                                            this.send(true);
-                                        }, 100);
-                                    } else if (xhr.status === 401) {
-                                        //console.log(xhr);
-                                        //console.log(!refresh_exp || leftRe < 0 || retryRequest);
-
-                                        // if refresh_token expired, does not exist, or retryRequest failed
-                                        if (!refresh_exp || leftRe < 0 || retryRequest) {
-                                            // Refresh token expired
-                                            me.logoutAndExit();
-                                        } else {
-                                            //console.log(me.checkRefStatus, refresh_exp !== me.get('refresh_exp'));
-                                            setTimeout(() => {
-                                                if (me.checkRefStatus || refresh_exp !== me.get('refresh_exp')) {
-                                                    this.send(true);
-                                                } else {
-                                                    me.logoutAndExit();
-                                                }
-                                            }, 200);
-                                        }
+                                            this.send(0);
+                                        }, 200);
+                                    } else if (xhr.status === 401 && retryRequest < 2) {
+                                        me.checkRefStatus = true;
+                                        me.refresh()
+                                            .then(() => this.send(retryRequest + 1))
+                                            .catch(e => me.logoutAndWarn(e))
+                                            .then(() => me.checkRefStatus = false);
+                                    } else {
+                                        this.abort();
                                     }
-
                                 });
                             }
                         };
                         return {
                             send: function (headers, cb) {
                                 transport.cb = cb;
-                                transport.send(false);
+                                transport.send(0);
                             },
                             abort: function () {
-                                transport.cb(400)
+                                transport.abort();
                             }
                         };
                     }
                 });
 
-                /*$( document ).ajaxError(function (model, jqXHR) {
-                    const now = moment().valueOf(),
-                        refresh_exp = me.get('refresh_exp'),
-                        leftRe = refresh_exp && (refresh_exp - now);
-
-                    if (!refresh_exp || leftRe < 0) {
-                        // Refresh token expired
-                        me.logoutAndExit();
-                    } else if (jqXHR.status === 401) {
-                        setTimeout(() => {
-                            if(me.checkRefStatus || refresh_exp !== me.get('refresh_exp')){
-
-                            }
-                        }, 200);
-                    }
-                });*/
-
                 var originalNavigate = Backbone.history.navigate;
                 Backbone.history.navigate = function (fragment, options) {
                     originalNavigate.apply(this, arguments);
-                    me.chRef();
-                }
+                };
             },
-            logoutAndExit: function() {
+            loggingOut: false,
+            logoutAndWarn: function (e) {
                 var me = this;
+                if (me.loggingOut) return;
+
+                const message = e && e.responseJSON && e.responseJSON.error_description;
+                me.loggingOut = true;
+
                 me.logout().then(
                     function () {
-                        if (!window.BackboneApp) return window.open('login',"_self",false);
+                        if (!window.BackboneApp) return window.open('login', "_self", false);
                         const router = window.BackboneApp.getRouter();
                         router.navigate('login', {
                             trigger: true
                         });
-                        AlertView.show('Warning', 'Your access has expired', 'warning');
-                        setTimeout(() => location.reload(), 350);
+                        AlertView.show('Warning', message || 'Your access has expired', 'warning');
+                        setTimeout(() => me.loggingOut = false, 300);
                     }
                 );
             },
             checkRefStatus: false,
-            checkRefresh: function (jqXHR) {
+            checkRefresh: function () {
                 var me = this;
                 if (!window.BackboneApp || me.checkRefStatus) return;
+
                 me.checkRefStatus = true;
-                me.getAuth().then(function () {
-                    //if(!me.checkLogout(jqXHR)) {
-                        me.ref();
-                    //}
-                })
-                    .catch(_.noop)
+                me.getAuth()
+                    .then(function () {
+                        const now = moment().valueOf(),
+                            access_exp = me.get('access_exp'),
+                            left = access_exp && (access_exp - now),
+                            refresh_exp = me.get('refresh_exp'),
+                            leftRe = (refresh_exp - now);
+                        if (!refresh_exp || leftRe < 0) {
+                            me.logoutAndWarn();
+                        } else if (!access_exp || left < 0) {
+                            return me.refresh()
+                                .then(_.noop);
+                        }
+                    })
+                    .catch(e => e && me.logoutAndWarn(e))
                     .then(function () {
                         me.checkRefStatus = false
                     });
             },
-            /*checkLogout: function(jqXHR) {
-                var me = this;
-                const now = moment().valueOf(),
-                    access_exp = me.get('access_exp'),
-                    refresh_exp = me.get('refresh_exp'),
-                    router = window.BackboneApp.getRouter(),
-                    left = (access_exp - now),
-                    leftRe = (refresh_exp - now);
-                if (!refresh_exp || leftRe < 0) {
-                    me.logout().then(
-                        function () {
-                            router.navigate('login', {
-                                trigger: true
-                            });
-                            AlertView.show('Warning', 'Access expired by refresh timeout, logged out.', 'warning');
-                        }
-                    );
-                    jqXHR && jqXHR.abort();
-                    return true;
-                } else if (!access_exp || left < 0) {
-                    me.logout().then(
-                        function () {
-                            router.navigate('login', {
-                                trigger: true
-                            });
-                            AlertView.show('Warning', 'Access expired by inactivity timeout, logged out.', 'warning');
-                        }
-                    );
-                    jqXHR && jqXHR.abort();
-                    return true;
-                }
-                return false;
-            },*/
             login: function (creds) {
                 var me = this;
                 return new Promise(function (res, rej) {
@@ -234,21 +198,21 @@
                     me.loginModel.getRefresh(rt)
                         .then(function (resp) {
                             me.parseResp(resp);
+                            me.save();
                             me.getAuth().then(res).catch(rej);
                         })
                         .catch(rej);
                 })
             },
             parseResp: function (resp) {
-                if(typeof resp === 'string') {
+                if (typeof resp === 'string') {
                     try {
                         const json = JSON.parse(resp);
                         resp = json;
-                    } catch(e){
+                    } catch (e) {
 
                     }
                 }
-                //console.log(2, resp);
                 var me = this;
                 var access_token = resp && resp['access_token'] && jwtDecode(resp['access_token']);
                 var refresh_token = resp && resp['refresh_token'] && jwtDecode(resp['refresh_token']);
@@ -271,7 +235,6 @@
                         refresh_token: resp['refresh_token'],
                         refresh_exp: refresh_exp
                     });
-                    //me.chRef();
                 } else {
                     me.set({
                         auth: false,
@@ -290,43 +253,6 @@
                         refresh_exp: ''
                     });
                 }
-            },
-            refreshActive: false,
-            refreshTimeout: null,
-            clearRunner: function () {
-                var me = this;
-                clearTimeout(me.refreshTimeout);
-                me.refreshActive = false;
-            },
-            refreshRunner: function (timeout) {
-                var me = this;
-                if (me.refreshActive) return;
-                me.clearRunner();
-                me.getAuth().then(function () {
-                    me.refreshActive = true;
-                    me.refreshTimeout = setTimeout(function () {
-                        const now = moment().valueOf(),
-                            access_exp = me.get('access_exp'),
-                            left = (access_exp - now),
-                            min = 60 * 1000;
-                        if (left > 17 * min) {
-                            // Uncomment if refresh is needed during an inactivity too
-                            //me.refreshRunner(10 * min);
-                            me.refreshActive = false;
-                        } else if (left > min) {
-                            me.refreshRunner(left - min);
-                            me.refreshActive = false;
-                        } else {
-                            me.refresh()
-                                .then(_.noop)
-                                .catch(_.noop)
-                                // finally
-                                .then(function () {
-                                    me.refreshActive = false;
-                                })
-                        }
-                    }, timeout);
-                });
             },
             getAuth: function () {
                 var me = this;
